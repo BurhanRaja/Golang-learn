84ac1503e13f7a481228a09fd9e4840fd68d91ab
BurhanuddinRaja <mails4burhan@gmail.com>
2023-02-09 09:28:59 +0530

    added functions

--- main.go
+++ main.go
@@ -25,47 +25,17 @@ func main() {
 
 	// Infinite For Loop
 	for {
-		var firstName string
-		var lastName string
-		var userEmail string
-		var userTicket uint
+		firstName, lastName, userEmail, userTicket := inputUser()
 
-		fmt.Print("Enter your first name: \n")
-		fmt.Scan(&firstName) // passing a pointer instead of a userName variable
-		fmt.Print("Enter your last name: \n")
-		fmt.Scan(&lastName) // passing a pointer instead of a userName variable
-		fmt.Print("Enter your email name: \n")
-		fmt.Scan(&userEmail) // passing a pointer instead of a userName variable
-		fmt.Print("Enter the no of tickets required: \n")
-		fmt.Scan(&userTicket) // passing a pointer instead of a userName variable
-
-		// ? Validation
-		isValidName := len(firstName) >= 2 && len(lastName) >= 2
-		isValidEmail := strings.Contains(userEmail, "@.")
-		isValidTicket := userTicket <= remainingTickets && userTicket > 0
+		isValidName, isValidEmail, isValidTicket := validateUserInp(firstName, lastName, userEmail, userTicket, remainingTickets)
 
 		if isValidName && isValidEmail && isValidTicket {
-			remainingTickets -= userTicket
 
-			// ? Array Based
-			// bookings[0] = firstName + " " + lastName
-			// fmt.Printf("The whole array: %v \n", bookings)
-			// fmt.Printf("The first value: %v \n", bookings[0])
-			// fmt.Printf("The Array Type: %T \n", bookings)
-			// fmt.Printf("The Array Size: %v \n", len(bookings))
-
-			// ? Slice Based
-			bookings = append(bookings, firstName+" "+lastName)
-			fmt.Printf("The whole slic: %v \n", bookings)
-			fmt.Printf("The first value: %v \n", bookings[0])
-			fmt.Printf("The Slice Type: %T \n", bookings)
-			fmt.Printf("The Slice Size: %v \n", len(bookings))
-
-			fmt.Printf("Thank you %v %v for booking %v tickets. You will recieve a confirmation email at %v  \n", firstName, lastName, userTicket, userEmail)
-			fmt.Printf("%v tickets are remaining for %v", remainingTickets, conferenceName)
+			bookingDone(remainingTickets, userTicket, bookings, firstName, lastName, userEmail, conferenceName)
 
 			// Get First Names
-			getFirstNames(bookings)
+			firstNames := getFirstNames(bookings)
+			fmt.Printf("The first names o the bookings are: %v\n", firstNames)
 
 			// ? If/Else
 			if remainingTickets == 0 {
@@ -104,7 +74,7 @@ func greetUser(confName string, confTickets uint, remainTickets uint) {
 	fmt.Println("Get your ticket from here to attend.")
 }
 
-func getFirstNames(bookings []string) {
+func getFirstNames(bookings []string) []string {
 	// ? For Loop
 	firstNames := []string{}
 	for _, booking := range bookings {
@@ -112,5 +82,54 @@ func getFirstNames(bookings []string) {
 		firstNames = append(firstNames, names[0])
 	}
 
-	fmt.Printf("The first names o the bookings are: %v\n", firstNames)
+	return firstNames
+}
+
+func validateUserInp(firstName string, lastName string, userEmail string, userTicket uint, remainingTickets uint) (bool, bool, bool) {
+	// ? Validation
+	isValidName := len(firstName) >= 2 && len(lastName) >= 2
+	isValidEmail := strings.Contains(userEmail, "@.")
+	isValidTicket := userTicket <= remainingTickets && userTicket > 0
+
+	return isValidName, isValidEmail, isValidTicket
+}
+
+func inputUser() (string, string, string, uint) {
+	var firstName string
+	var lastName string
+	var userEmail string
+	var userTicket uint
+
+	fmt.Print("Enter your first name: \n")
+	fmt.Scan(&firstName) // passing a pointer instead of a userName variable
+	fmt.Print("Enter your last name: \n")
+	fmt.Scan(&lastName) // passing a pointer instead of a userName variable
+	fmt.Print("Enter your email name: \n")
+	fmt.Scan(&userEmail) // passing a pointer instead of a userName variable
+	fmt.Print("Enter the no of tickets required: \n")
+	fmt.Scan(&userTicket) // passing a pointer instead of a userName variable
+
+	return firstName, lastName, userEmail, userTicket
+}
+
+func bookingDone(remainingTickets uint, userTicket uint, bookings []string, firstName string, lastName string, userEmail string, conferenceName string) {
+	
+	remainingTickets -= userTicket
+
+	// ? Array Based
+	// bookings[0] = firstName + " " + lastName
+	// fmt.Printf("The whole array: %v \n", bookings)
+	// fmt.Printf("The first value: %v \n", bookings[0])
+	// fmt.Printf("The Array Type: %T \n", bookings)
+	// fmt.Printf("The Array Size: %v \n", len(bookings))
+
+	// ? Slice Based
+	bookings = append(bookings, firstName+" "+lastName)
+	// fmt.Printf("The whole slic: %v \n", bookings)
+	// fmt.Printf("The first value: %v \n", bookings[0])
+	// fmt.Printf("The Slice Type: %T \n", bookings)
+	// fmt.Printf("The Slice Size: %v \n", len(bookings))
+
+	fmt.Printf("Thank you %v %v for booking %v tickets. You will recieve a confirmation email at %v  \n", firstName, lastName, userTicket, userEmail)
+	fmt.Printf("%v tickets are remaining for %v", remainingTickets, conferenceName)
 }
